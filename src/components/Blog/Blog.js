147a312289e2b35aeb41b0d7e16e9b67f2ef3f01
mdrayhanhowlader <rhowlader5314@gmail.com>
2022-10-12 22:42:07 +0600

    blog page updated

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -6,15 +6,15 @@ const Blog = () => {
         <div>
             <div className='w-4/5 mx-auto'>
             <div className='bg-slate-300 rounded my-6 p-4'>
-                <h2 className='font-semibold text-2xl'>How does React works!</h2>
+                <h2 className='font-semibold text-2xl'>What is the purpose of react routes?</h2>
                 <p>React uses a declarative paradigm that makes it easier to reason about your application and aims to be both efficient and flexible. It designs simple views for each state in your application, and React will efficiently update and render just the right component when your data changes.</p>
             </div>
             <div className='bg-slate-300 rounded my-6 p-4'>
-                <h2 className='font-semibold text-2xl'>Between difference props and state</h2>
+                <h2 className='font-semibold text-2xl'>How does context api works?</h2>
                 <p>Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.</p>
             </div>
             <div className='bg-slate-300 rounded my-6 p-4'>
-                <h2 className='font-semibold text-2xl'>How many ways use useEffect without loading data from api?</h2>
+                <h2 className='font-semibold text-2xl'>What is useRef and how does works?</h2>
                 <p>loading..............................</p>
             </div>
             </div>
